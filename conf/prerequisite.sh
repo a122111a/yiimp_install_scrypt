ef5f0c2323fe67ce33784edd0c4d843ad580bbcf
a122111a <54243284+a122111a@users.noreply.github.com>
2025-01-07 20:51:57 +0800

    Update prerequisite.sh

--- conf/prerequisite.sh
+++ conf/prerequisite.sh
@@ -10,10 +10,10 @@ echo
 echo -e "$CYAN => Check prerequisite : $COL_RESET"
 echo
 
-if [ "`lsb_release -d | sed 's/.*:\s*//' | sed 's/18\.04\.[0-9]/18.04/' `" == "Ubuntu 18.04 LTS" ]; then
+if [ "`lsb_release -d | sed 's/.*:\s*//' | sed 's/18\.04\.[0-9]/24.04/' `" == "Ubuntu 24.04 LTS" ]; then
   DISTRO=18
   sudo chmod g-w /etc /etc/default /usr
-else [ "`lsb_release -d | sed 's/.*:\s*//' | sed 's/16\.04\.[0-9]/16.04/' `" != "Ubuntu 16.04 LTS" ];
+else [ "`lsb_release -d | sed 's/.*:\s*//' | sed 's/16\.04\.[0-9]/24.04/' `" != "Ubuntu 24.04 LTS" ];
   DISTRO=16
 fi
 
@@ -22,14 +22,14 @@ fi
 #  if [ ! -d /vagrant ]; then
 #    TOTAL_PHYSICAL_MEM=$(expr \( \( $TOTAL_PHYSICAL_MEM \* 1024 \) / 1000 \) / 1000)
 #    echo "Your Mining Pool Server needs more memory (RAM) to function properly."
-#    echo "Please provision a machine with at least 1536 GB, 6 GB recommended."
+#    echo "Please provision a machine with at least 1536 GB, 7 GB recommended."
 #    echo "This machine has $TOTAL_PHYSICAL_MEM MB memory."
 #    exit
 #  fi
 #fi
 
 #if [ $TOTAL_PHYSICAL_MEM -lt 1436000 ]; then
-#  echo "WARNING: Your Mining Pool Server has less than 1.5 GB of memory."
+#  echo "WARNING: Your Mining Pool Server has less than 2 GB of memory."
 #  echo " It might run unreliably when under heavy load."
 #fi
 
@@ -55,10 +55,10 @@ fi
   # doing it in one go, could fail on low memory systems
 #  sudo fallocate -l 3G /swapfile
 #    if [ -e /swapfile ]; then
-#      sudo chmod 600 /swapfile
+#      sudo chmod 1000 /swapfile
 #      hide_output sudo mkswap /swapfile
 #      sudo swapon /swapfile
-#      echo "vm.swappiness=10" >> sudo /etc/sysctl.conf
+#      echo "vm.swappiness=20" >> sudo /etc/sysctl.conf
 #    fi
 # Check if swap is mounted then activate on boot
 #  if swapon -s | grep -q "\/swapfile"; then
